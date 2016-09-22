61f31e9a9ebf0c59210863e94736bda0ff242d97
Takaaki ISHIKAWA <takaxp@ieee.org>
2016-09-22 18:14:26 +0900

    Support Shift+Space

--- cmd-eikana/KeyEvent.swift
+++ cmd-eikana/KeyEvent.swift
@@ -10,6 +10,7 @@ import Cocoa
 
 class KeyEvent: NSObject {
     var keyCode: UInt16? = nil
+    var imeStatus: Bool = false
     
     override init() {
         super.init()
@@ -22,6 +23,8 @@ class KeyEvent: NSObject {
             Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(KeyEvent.watchAXIsProcess(_:)), userInfo: nil, repeats: true)
             
         } else {
+
+            self.setEisu()
             self.watch()
         }
     }
@@ -29,15 +32,35 @@ class KeyEvent: NSObject {
     func watchAXIsProcess(_ timer: Timer) {
         if AXIsProcessTrusted() {
             timer.invalidate()
+            #if DEBUG
             print("アクセシビリティに設定されました")
+            #endif
             
             self.watch()
-            
+
             addLaunchAtStartup()
             loginItem.state = 1
         }
     }
-    
+
+    func setEisu() {
+        let loc = CGEventTapLocation.cghidEventTap
+        CGEvent(keyboardEventSource: nil, virtualKey: 102, keyDown: true)?.post(tap: loc)
+        CGEvent(keyboardEventSource: nil, virtualKey: 102, keyDown: false)?.post(tap: loc)
+    }
+
+    func setKana() {
+        let loc = CGEventTapLocation.cghidEventTap
+        CGEvent(keyboardEventSource: nil, virtualKey: 104, keyDown: true)?.post(tap: loc)
+        CGEvent(keyboardEventSource: nil, virtualKey: 104, keyDown: false)?.post(tap: loc)
+    }
+
+    func removeSpace() {
+        let loc = CGEventTapLocation.cghidEventTap
+        CGEvent(keyboardEventSource: nil, virtualKey: 51, keyDown: true)?.post(tap: loc)
+        CGEvent(keyboardEventSource: nil, virtualKey: 51, keyDown: false)?.post(tap: loc)
+    }
+
     func watch () {
         let masks = [
             NSEventMask.keyDown,
@@ -59,37 +82,77 @@ class KeyEvent: NSObject {
             NSEvent.addGlobalMonitorForEvents(matching: mask, handler: handler)
         }
         
+        NSEvent.addGlobalMonitorForEvents(matching: NSEventMask.keyDown , handler: {(event: NSEvent!) -> Void in
+
+            #if DEBUG
+            print(self.keyCode)
+            #endif
+
+            if ( event.keyCode != 49 ){
+                return;
+            }
+            if ( !event.modifierFlags.contains(.shift)
+                || event.modifierFlags.contains(.command)
+                || event.modifierFlags.contains(.option)
+                || event.modifierFlags.contains(.control)){
+                return;
+            }
+
+            #if DEBUG
+            print("value: ", event)
+            #endif
+
+            self.removeSpace()
+
+            if self.imeStatus == true {
+                // IME off
+                self.setEisu()
+                self.imeStatus = false;
+                #if DEBUG
+                print("ime OFF")
+                #endif
+            }else{
+                // IME on
+                self.setKana()
+                self.imeStatus = true;
+                #if DEBUG
+                print("ime ON")
+                #endif
+            }
+        })
+
+
+        #if true
         NSEvent.addGlobalMonitorForEvents(matching: NSEventMask.flagsChanged, handler: {(evevt: NSEvent!) -> Void in
-            if evevt.keyCode == 55 { // 右コマンドキー
+            if evevt.keyCode == 55 { // 左コマンドキー
                 if evevt.modifierFlags.contains(.command) {
                     self.keyCode = 55
                 }
                 else if self.keyCode == 55 {
+                    #if DEBUG
                     print("英数")
-                    
-                    let loc = CGEventTapLocation.cghidEventTap
-                    
-                    CGEvent(keyboardEventSource: nil, virtualKey: 102, keyDown: true)?.post(tap: loc)
-                    CGEvent(keyboardEventSource: nil, virtualKey: 102, keyDown: false)?.post(tap: loc)
+                    #endif
+
+                    self.setEisu()
                 }
             }
-            else if evevt.keyCode == 54 { // 左コマンドキー
+            else if evevt.keyCode == 54 { // 右コマンドキー
                 if evevt.modifierFlags.contains(.command) {
                     self.keyCode = 54
                 }
                 else if self.keyCode == 54 {
+                    #if DEBUG
                     print("かな")
-                    
-                    let loc = CGEventTapLocation.cghidEventTap
-                    
-                    CGEvent(keyboardEventSource: nil, virtualKey: 104, keyDown: true)?.post(tap: loc)
-                    CGEvent(keyboardEventSource: nil, virtualKey: 104, keyDown: false)?.post(tap: loc)
+                    #endif
+
+                    self.setKana()
                 }
             }
             else {
-                self.keyCode = nil
+                self.keyCode = nil;
             }
         })
+        #endif
         
     }
 }
